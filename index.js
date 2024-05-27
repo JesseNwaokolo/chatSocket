7bebbc9180d851593fc7f4045f6b9c26ac0528c8
Jesse <jeschinwa@gmail.com>
2024-05-27 13:31:28 -0700

    second

---
+++ index.js
@@ -0,0 +1,40 @@
+const { Server } = require("socket.io");
+
+const io = new Server({ cors: "http://localhost:5173" });
+
+let onlineUser = [];
+
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id);
+
+  socket.on("addnewuser", (userId) => {
+    !onlineUser.some((user) => {
+      user.userId === userId;
+    }) &&
+      onlineUser.push({
+        userId,
+        socketId: socket.id,
+      });
+
+      io.emit("getOnlineUser", onlineUser)
+  });
+
+  socket.on("sendMessage", (message)=>{
+    const user = onlineUser.find(user => user.userId === message.recipientId)
+    if (user){
+        io.to(user.socketId).emit("getMessage", message)
+        io.to(user.socketId).emit("getNotif", {
+            senderId : message.senderId,
+            isRead:false,
+            date:new Date()
+        })
+    }
+  })
+
+  socket.on("disconnect", ()=>{
+    onlineUser = onlineUser.filter(user => user.socketId !== socket.id)
+    io.emit("getOnlineUser", onlineUser)
+  })
+});
+
+io.listen(3000);
